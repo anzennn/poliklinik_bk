1061bc5a3ad578ed0e8c4e22ca98468eae811ea1
anzennn <amanullahfl13@gmail.com>
2023-12-27 11:22:41 +0700

    Tugas BK

---
+++ poliklinik_new.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 16, 2023 at 10:49 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `poliklinik_new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar_poli`
+--
+
+CREATE TABLE `daftar_poli` (
+  `id` int(11) UNSIGNED DEFAULT NULL,
+  `id_pasien` int(11) UNSIGNED DEFAULT NULL,
+  `id_jadwal` int(11) UNSIGNED DEFAULT NULL,
+  `keluhan` text DEFAULT NULL,
+  `no_antrian` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_periksa`
+--
+
+CREATE TABLE `detail_periksa` (
+  `id` int(11) UNSIGNED DEFAULT NULL,
+  `id_periksa` int(11) UNSIGNED DEFAULT NULL,
+  `id_obat` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `id` int(11) UNSIGNED DEFAULT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `no_hp` varchar(50) DEFAULT NULL,
+  `id_poli` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal_periksa`
+--
+
+CREATE TABLE `jadwal_periksa` (
+  `id` int(11) UNSIGNED DEFAULT NULL,
+  `id_dokter` int(11) UNSIGNED DEFAULT NULL,
+  `hari` enum('Senin','Selasa','Rabu','Kamis','Jumat','Sabtu') DEFAULT NULL,
+  `jam_mulai` time DEFAULT NULL,
+  `jam_selesai` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jadwal_periksa`
+--
+
+INSERT INTO `jadwal_periksa` (`id`, `id_dokter`, `hari`, `jam_mulai`, `jam_selesai`) VALUES
+(NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id` int(11) UNSIGNED DEFAULT NULL,
+  `nama_obat` varchar(50) DEFAULT NULL,
+  `kemasan` varchar(35) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id`, `nama_obat`, `kemasan`, `harga`) VALUES
+(1, 'paramex', 'tablet', 7000),
+(2, 'panadol', 'sachet', 3000),
+(3, 'procold', 'tablet', 5000),
+(4, 'bodrex', 'botol', 12000),
+(9, 'Panadol', 'Saset', 7000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` int(11) UNSIGNED DEFAULT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `no_ktp` varchar(255) DEFAULT NULL,
+  `no_hp` varchar(50) DEFAULT NULL,
+  `no_rm` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `periksa`
+--
+
+CREATE TABLE `periksa` (
+  `id` int(11) UNSIGNED DEFAULT NULL,
+  `id_daftar_poli` int(11) UNSIGNED DEFAULT NULL,
+  `tgl_periksa` datetime DEFAULT NULL,
+  `biaya_periksa` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `poli`
+--
+
+CREATE TABLE `poli` (
+  `id` int(11) UNSIGNED DEFAULT NULL,
+  `nama_poli` varchar(25) DEFAULT NULL,
+  `keterangan` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
